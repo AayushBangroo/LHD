e83dd9f248db27d0934f0a703a1b8a30156420b3
Your Name <you@example.com>
2021-01-13 16:29:39 +0530

    sort a list

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+const arr = [5, 4, 3, 2, 1];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = 0; j < arr.length - i - 1; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+
+console.log(arr); //1 2 3 4 5
